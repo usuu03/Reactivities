db92635a818c962bca4b15de8f21b4c119626c80
usuu03 <usunobu.omijie@gmail.com>
2025-05-04 18:49:20 +0100

    Add new dependencies for form handling and validation; update ActivityForm and ActivityDetailsHeader components for improved functionality

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,85 +1,91 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import { FormEvent } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
 import { useNavigate, useParams } from "react-router";
-
+import { useForm, type FieldValues } from "react-hook-form";
+import { useEffect } from "react";
 
 export default function ActivityForm() {
-  const { id } = useParams();
-  const { updateActivity, createActivity, activity, isLoadingActivty } = useActivities(id);
-  const navigate = useNavigate();
+	const { register, reset, handleSubmit } = useForm();
+	const { id } = useParams();
+	const { updateActivity, createActivity, activity, isLoadingActivty } =
+		useActivities(id);
+	const navigate = useNavigate();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+	useEffect(() => {
+		if (activity) reset(activity);
+	}, [activity, reset]);
 
-    const formData = new FormData(event.currentTarget);
+	const onSubmit = async (data: FieldValues) => {
+		console.log(data);
+	};
 
-    const data: { [key: string]: FormDataEntryValue } = {};
-    formData.forEach((value, key) => {
-      data[key] = value;
-    });
-
-    if (activity) {
-      data.id = activity.id
-      await updateActivity.mutateAsync(data as unknown as Activity);
-      navigate(`/activities/${activity.id}`);
-    } else {
-      createActivity.mutateAsync(data as unknown as Activity, {
-        onSuccess: (id) => {
-          navigate(`/activities/${id}`);
-        }
-      });
-    }
-
-  };
-
-  if (isLoadingActivty) return <Typography>Loading activity...</Typography>;
-  return (
-    <Paper sx={{ padding: 3, borderRadius: 3 }}>
-      <Typography variant="h5" gutterBottom color="primary">
-        {activity ? "Edit Activity" : "Create Activity"}
-      </Typography>
-      <Box
-        component="form"
-        onSubmit={handleSubmit}
-        display="flex"
-        flexDirection="column"
-        gap={3}
-      >
-        <TextField name="title" label="Title" defaultValue={activity?.title} />
-        <TextField
-          name="description"
-          label="Description"
-          defaultValue={activity?.description}
-          multiline
-          rows={3}
-        />
-        <TextField
-          name="category"
-          label="Category"
-          defaultValue={activity?.category}
-        />
-        <TextField
-          name="date"
-          label="Date"
-          type="date"
-          defaultValue={activity?.date ? new Date(activity.date).toISOString().split("T")[0] : new Date().toISOString().split("T")[0]}
-        />
-        <TextField name="city" label="City" defaultValue={activity?.city} />
-        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
-        <Box display="flex" justifyContent="end" gap={3}>
-          <Button color="inherit" onClick={() => navigate(`/activities/${activity?.id}`)}>
-            Cancel
-          </Button>
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            disabled={updateActivity.isPending || createActivity.isPending}>
-            Submit
-          </Button>
-        </Box>
-      </Box>
-    </Paper>
-  );
+	if (isLoadingActivty) return <Typography>Loading activity...</Typography>;
+	return (
+		<Paper sx={{ padding: 3, borderRadius: 3 }}>
+			<Typography variant="h5" gutterBottom color="primary">
+				{activity ? "Edit Activity" : "Create Activity"}
+			</Typography>
+			<Box
+				component="form"
+				onSubmit={handleSubmit(onSubmit)}
+				display="flex"
+				flexDirection="column"
+				gap={3}
+			>
+				<TextField
+					{...register("title")}
+					label="Title"
+					defaultValue={activity?.title}
+				/>
+				<TextField
+					{...register("description")}
+					label="Description"
+					defaultValue={activity?.description}
+					multiline
+					rows={3}
+				/>
+				<TextField
+					{...register("category")}
+					label="Category"
+					defaultValue={activity?.category}
+				/>
+				<TextField
+					{...register("date")}
+					label="Date"
+					type="date"
+					defaultValue={
+						activity?.date
+							? new Date(activity.date).toISOString().split("T")[0]
+							: new Date().toISOString().split("T")[0]
+					}
+				/>
+				<TextField
+					{...register("city")}
+					label="City"
+					defaultValue={activity?.city}
+				/>
+				<TextField
+					{...register("venue")}
+					label="Venue"
+					defaultValue={activity?.venue}
+				/>
+				<Box display="flex" justifyContent="end" gap={3}>
+					<Button
+						color="inherit"
+						onClick={() => navigate(`/activities/${activity?.id}`)}
+					>
+						Cancel
+					</Button>
+					<Button
+						type="submit"
+						variant="contained"
+						color="primary"
+						disabled={updateActivity.isPending || createActivity.isPending}
+					>
+						Submit
+					</Button>
+				</Box>
+			</Box>
+		</Paper>
+	);
 }
